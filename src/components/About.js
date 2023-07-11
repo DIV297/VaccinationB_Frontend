3a74feeba2d02bbf2b878d62f5adcd24cf37e7df
DIV297 <91648671+DIV297@users.noreply.github.com>
2023-07-11 15:39:01 +0530

    aboutfinal

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div className='container ' style={{'margin':'10vh'}}>
+    <div className='container ' style={{}}>
       <h3>This project is related to Covid Vaccination Booking.
       You can book your slots online with easy click by choosing your date and centers that are near to you. Login to apply for slot.
       </h3>
